0dac60ac1d73fa4a6ef5fdfa0e4f67bb1cf9fb82
Onoshe <adesung2005@yahoo.co.uk>
2020-10-02 16:35:42 +0100

    New

--- js/sonyApp.js
+++ js/sonyApp.js
@@ -12,6 +12,7 @@ function changePage2BtnColor(){
 var  tax_result = document.getElementsByClassName("taxResult");
 var  tax_Details = document.getElementsByClassName("taxDetails");
 var tax_Data = document.getElementsByClassName("taxdata");
+var tax_Band = document.getElementsByClassName("grid3-Cont-S3");
 //var tax_ResultCheck = document.getElementById("taxResultCheck");
 
 $(function(){
@@ -85,8 +86,9 @@ $(function(){
 	//Call payeeCalculator Function
 	var tax_Result = payeeCalculator(pension, nHF, bAllw, hAllw, tAllw, oAllw, nAllw, annualSel, monthlySelect, annualAllwSel, monthlyAllwSelect);
 	//returned tax_Result is [tgrossPay, taxableIncome, netPay,  penChr, nHFChr, taxPayable, minTax];
-	//returned tax_Result is = [tgrossPayAnnual, taxableIncomeAnnual, netPayAnnual,  penChrAnnual, nHFChrAnnual, taxPayableAnnual, minTaxAnnual, + 
-			//fConsolRf, first300, next600, next1100, next1600, next3200, over3200, tgrossPayAnnual, taxableIncomeAnnual, nonTaxable, minTaxAnnual, taxPayableAnnual, aEffectTax, aDeductAllw, aDeductAndRelief];
+	//returned tax_Result is = [0.tgrossPayAnnual, 1.taxableIncomeAnnual, 2.netPayAnnual,  3.penChrAnnual, 4.nHFChrAnnual, 5.taxPayableAnnual, 6.minTaxAnnual, + 
+			//7.fConsolRf, 8.first300, 9.next600, 10.next1100, 11.next1600, 12.next3200, 13.over3200, 14.tgrossPayAnnual, 15.taxableIncomeAnnual, 16.nonTaxable, + 
+			// 17.minTaxAnnual, 18.taxPayableAnnual, 19.aEffectTax, 20.aDeductAllw, 21.aDeductAndRelief, 22.afirstTxbl300, 23.anextTxbl600, 24.anextTxbl1100, 25.anextTxbl1600, 26.anextTxbl3200, 27.aoverTxbl3200];
 	//tax_ResultCheck.innerHTML = tax_Result;
 	//alert(tax_Result);
 	$('#taxResultCheck').text(tax_Result);
@@ -102,19 +104,30 @@ $(function(){
 	//Tax Details
 	tax_Details[0].innerHTML = tax_Result[14];
 	tax_Details[1].innerHTML = tax_Result[16];
-	tax_Details[2].innerHTML = tax_Result[20];
-	tax_Details[3].innerHTML = tax_Result[7];  //Tax_Result[7] - ConsolRelf
-	tax_Details[4].innerHTML = tax_Result[21];
-	tax_Details[5].innerHTML = tax_Result[15];
-	tax_Details[6].innerHTML = tax_Result[18];
-	tax_Details[7].innerHTML = tax_Result[17];
+	tax_Details[2].innerHTML = tax_Result[3];
+	tax_Details[3].innerHTML = tax_Result[4];
 
-	tax_Details[8].innerHTML = tax_Result[8];  //First N300,000
-	tax_Details[9].innerHTML = tax_Result[9];
-	tax_Details[10].innerHTML = tax_Result[10];
-	tax_Details[11].innerHTML = tax_Result[11];
-	tax_Details[12].innerHTML = tax_Result[12];
-	tax_Details[13].innerHTML = tax_Result[13];
+	tax_Details[4].innerHTML = tax_Result[20]; //Tax_Result[] - Total deductions
+	tax_Details[5].innerHTML = tax_Result[7];  //Tax_Result[7] - ConsolRelf
+	tax_Details[6].innerHTML = tax_Result[21];
+	tax_Details[7].innerHTML = tax_Result[15];
+	tax_Details[8].innerHTML = tax_Result[18];
+	tax_Details[9].innerHTML = tax_Result[17];
+
+	tax_Details[10].innerHTML = tax_Result[8];  //First N300,000
+	tax_Details[11].innerHTML = tax_Result[9];
+	tax_Details[12].innerHTML = tax_Result[10];
+	tax_Details[13].innerHTML = tax_Result[11];
+	tax_Details[14].innerHTML = tax_Result[12];
+	tax_Details[15].innerHTML = tax_Result[13];
+
+	//Taxable Income Band
+	tax_Band[0].innerHTML = tax_Result[22]+' @7%';  //Taxable Income: First N300,000
+	tax_Band[1].innerHTML = tax_Result[23]+' @11%';
+	tax_Band[2].innerHTML = tax_Result[24]+' @15%';
+	tax_Band[3].innerHTML = tax_Result[25]+' @19%';
+	tax_Band[4].innerHTML = tax_Result[26]+' @21%';
+	tax_Band[5].innerHTML = tax_Result[27]+' @24%';
 	}
 	});
 });
@@ -208,40 +221,43 @@ if (taxableIncome <= 0){
   taxPayable = ((taxableIncome - 3200000) * 0.24) + 560000;
   over3200 = taxPayable;
 }
+		
+		//Get Tax Payable Amount for Each Tax band:
 		var first300, first600, first1100, first1600, first3200, first6400;
+		var firstTxbl300, nextTxbl600, nextTxbl1100, nextTxbl1600, nextTxbl3200, overTxbl3200; //Get taxable amount for each tax band
 		//First Level: First N300,000
-        if(taxableIncome <= 0){first300 = 0};
-        if(taxableIncome < 300000){first300 = (0.07 * taxableIncome)}
-        else {first300 = 21000}
+        if(taxableIncome <= 0){firstTxbl300 = 0; first300 = 0};
+        if(taxableIncome < 300000){firstTxbl300 = taxableIncome; first300 = (0.07 * taxableIncome)}
+        else {firstTxbl300 = 300000; first300 = 21000}
         
         //Second Level: Next N300,000 
         var cum600 = taxableIncome - 300000;
-        if(cum600 <= 0){first600 = 0};
-        if(cum600 > 0 && cum600 <= 300000){first600 = 0.11* cum600}
-        if(cum600 > 300000){first600 = 33000};
+        if(cum600 <= 0){nextTxbl600 = 0; first600 = 0};
+        if(cum600 > 0 && cum600 <= 300000){nextTxbl600 = cum600; first600 = 0.11* cum600}
+        if(cum600 > 300000){nextTxbl600 = 300000; first600 = 33000};
 
          //Third Level: Next N500,000
         var cum1100 = taxableIncome - 600000;
-        if(cum1100 <= 0){first1100 = 0};
-        if(cum1100 > 0 && cum1100 <= 500000){first1100 = 0.15* cum1100}
-        if(cum1100 > 500000){first1100 = 75000};
+        if(cum1100 <= 0){nextTxbl1100 = 0; first1100 = 0};
+        if(cum1100 > 0 && cum1100 <= 500000){nextTxbl1100 = cum1100; first1100 = 0.15* cum1100}
+        if(cum1100 > 500000){nextTxbl1100 = 500000; first1100 = 75000};
 
          //Forth Level: Next N500,000
         var cum1600 = taxableIncome - 1100000;
-        if(cum1600 <= 0){first1600 = 0};
-        if(cum1600 > 0 && cum1600 <= 500000){first1600 = 0.19* cum1600}
-        if(cum1600 > 500000){first1600 = 95000};
+        if(cum1600 <= 0){nextTxbl1600 = 0; first1600 = 0};
+        if(cum1600 > 0 && cum1600 <= 500000){nextTxbl1600 = cum1600; first1600 = 0.19* cum1600}
+        if(cum1600 > 500000){nextTxbl1600 = 500000; first1600 = 95000};
 
          //Fifth Level: Next N1,600,000
         var cum3200 = taxableIncome - 1600000;
-        if(cum3200 <= 0){first3200 = 0};
-        if(cum3200 > 0 && cum3200 <= 1600000){first3200 = 0.21* cum3200}
-        if(cum3200 > 1600000){first3200 = 336000};
+        if(cum3200 <= 0){nextTxbl3200 = 0; first3200 = 0};
+        if(cum3200 > 0 && cum3200 <= 1600000){nextTxbl3200 = cum3200; first3200 = 0.21* cum3200}
+        if(cum3200 > 1600000){nextTxbl3200 = 1600000; first3200 = 336000};
 
          //Sixth Level: Next N3,200,000
         var cum6400 = taxableIncome - 3200000;
-        if(cum6400 <= 0){first6400 = 0};
-        if(cum6400 > 0 ){first6400 = 0.24* cum6400}
+        if(cum6400 <= 0){overTxbl3200 = 0; first6400 = 0};
+        if(cum6400 > 0 ){overTxbl3200 = cum6400; first6400 = 0.24* cum6400}
 
 //Minimum Tax?
 if(minTax > taxPayable){
@@ -255,12 +271,15 @@ if(minTax > taxPayable){
 //Detailed Analyses
 var aconsolRf, afirst300, anext600, anext1100, anext1600, anext3200, aover3200;
 aconsolRf = consolRf.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-afirst300 = first300.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
+/*afirst300 = first300.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); */
 anext600 = first600.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
 anext1100 = first1100.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 anext1600 = first1600.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 anext3200 = first3200.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 aover3200 = first6400.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
+if(first300 < 0){afirst300 = '0.00'}
+	else{afirst300 = first300.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}; 
 
 //Annual Result
 var effectTax, aEffectTax, deductAllw, aDeductAllw, deductAndRelief, aDeductAndRelief;
@@ -272,6 +291,17 @@ nHFChr = nHFChr;
 taxableIncome = taxableIncome;
 deductAllw = penChr + nHFChr;
 deductAndRelief = fConsolRF + deductAllw;
+
+var afirstTxbl300, anextTxbl600, anextTxbl1100, anextTxbl1600, anextTxbl3200, aoverTxbl3200;
+if(firstTxbl300 < 0){afirstTxbl300 = '0.00'}
+	else{afirstTxbl300 = firstTxbl300.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')};
+
+anextTxbl600 = nextTxbl600.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
+anextTxbl1100 = nextTxbl1100.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+anextTxbl1600 = nextTxbl1600.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+anextTxbl3200 = nextTxbl3200.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+aoverTxbl3200 = overTxbl3200.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
 
 tgrossPayAnnual = tgrossPay.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 netPayAnnual = netPay.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
@@ -287,7 +317,7 @@ aDeductAndRelief = deductAndRelief.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'
 if(taxableIncome < 0){taxableIncomeAnnual = 'Nil'}
 	else{taxableIncomeAnnual = taxableIncome.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')};
 
-annualFResult = [tgrossPayAnnual, taxableIncomeAnnual, netPayAnnual,  penChrAnnual, nHFChrAnnual, taxPayableAnnual, minTaxAnnual, fConsolRF, afirst300, anext600, anext1100, anext1600, anext3200, aover3200, tgrossPayAnnual, taxableIncomeAnnual, nonTaxable, minTaxAnnual, taxPayableAnnual, aEffectTax, aDeductAllw, aDeductAndRelief];
+annualFResult = [tgrossPayAnnual, taxableIncomeAnnual, netPayAnnual,  penChrAnnual, nHFChrAnnual, taxPayableAnnual, minTaxAnnual, fConsolRF, afirst300, anext600, anext1100, anext1600, anext3200, aover3200, tgrossPayAnnual, taxableIncomeAnnual, nonTaxable, minTaxAnnual, taxPayableAnnual, aEffectTax, aDeductAllw, aDeductAndRelief, afirstTxbl300, anextTxbl600, anextTxbl1100, anextTxbl1600, anextTxbl3200, aoverTxbl3200];
 //Minimum Tax format
 if(taxPayableAnnual === minTaxAnnual){
 	//alert('Minimumtax');
@@ -327,7 +357,7 @@ if(montaxableIncome < 0){montaxableIncome = 'Nil'}
 
 
 //annualFResult = [tgrossPayAnnual, taxableIncomeAnnual, netPayAnnual,  penChrAnnual, nHFChrAnnual, taxPayableAnnual, minTaxAnnual, consolRf, first300, next600, next1100, next1600, next3200, over3200, tgrossPayAnnual, taxableIncomeAnnual, nonTaxable, minTaxAnnual, taxPayableAnnual];
-monthFResult = [monGross, montaxableIncome, monNet, monPen, monNHF, monTax,  minTax, fConsolRF, afirst300, anext600, anext1100, anext1600, anext3200, aover3200, tgrossPayAnnual, taxableIncomeAnnual, nonTaxable, minTaxAnnual, taxPayableAnnual, fmEffectTax, aDeductAllw, aDeductAndRelief];
+monthFResult = [monGross, montaxableIncome, monNet, monPen, monNHF, monTax,  minTax, fConsolRF, afirst300, anext600, anext1100, anext1600, anext3200, aover3200, tgrossPayAnnual, taxableIncomeAnnual, nonTaxable, minTaxAnnual, taxPayableAnnual, fmEffectTax, aDeductAllw, aDeductAndRelief, afirstTxbl300, anextTxbl600, anextTxbl1100, anextTxbl1600, anextTxbl3200, aoverTxbl3200];
 
 if(annualResult ==="Yes"){
 	return annualFResult}
@@ -438,7 +468,7 @@ function addCommas(x){
 	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	return parts.join(".");
 }
-*/
+
 $(function(){
 	//var test = $('.navBtns-Home').css("margin");
 	$(".pitDetails").click(function(){
@@ -446,4 +476,4 @@ $(function(){
 	alert(x);
 	});
 });
-
+*/
